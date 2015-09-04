b2d61e7cf79c515819fdfddf93fd827d617c8234
Steve Masover <masover@airbears2-10-142-48-50.AirBears2.1918.Berkeley.EDU>
2015-09-04 15:22:54 -0700

    use variables instead of param numbers

--- boxcurl.sh
+++ boxcurl.sh
@@ -6,9 +6,8 @@ target=$3
 
 echo "User is box account (email address); source is folder/file relative to user's home dir on box; target is relative to current directory."
 echo ""
-echo "user:   " $1
-echo "src:    " $2
-echo "target: " $3
+echo "user:   " $user
+echo "src:    " $src
+echo "target: " $target
 
-curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u $1 --ftp-ssl ftps://ftp.box.com:990/$2 > $3
-
+curl -1 -v --disable-epsv --ftp-skip-pasv-ip -u $user --ftp-ssl ftps://ftp.box.com:990/$src > $target
